7d04d223420096c88d752f69420b00822cbb7ea7
Kevin Forero <kforero@dondoctor.com>
2021-11-02 12:00:07 -0500

    Funciones de creación de Mecanico

--- WebMVC/Controllers/MecanicosController.cs
+++ WebMVC/Controllers/MecanicosController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using WebMVC.WCFMecanicos;
@@ -20,5 +25,71 @@ namespace WebMVC.Controllers
             //return Json(Respuesta, JsonRequestBehavior.AllowGet);
             return View(lisaMecanicos);
         }
+
+        [HttpGet]
+        public ActionResult MecanicosDetail(string tipoDocumento, int? documento)
+        {
+            if (!string.IsNullOrEmpty(tipoDocumento) && documento.HasValue && documento.Value > 0)
+            {
+                List<WCFMecanicos.TMecanicos> listaMecanicos = new List<WCFMecanicos.TMecanicos>();
+                WCFMecanicos.MecanicosClient wcfMecanicos = new MecanicosClient();
+
+                listaMecanicos = wcfMecanicos.ConsultarMecanicos().Where(x => x.Tipo_Documento.Equals(tipoDocumento) && x.Documento.Equals(documento.Value)).ToList();
+
+                WebMVC.Models.EditMecanicos mecanico = new WebMVC.Models.EditMecanicos();
+
+                mecanico.Documento = listaMecanicos[0].Documento;
+                mecanico.Tipo_Documento = listaMecanicos[0].Tipo_Documento;
+                mecanico.Primer_Nombre = listaMecanicos[0].Primer_Nombre;
+                mecanico.Segundo_Apellido = listaMecanicos[0].Segundo_Apellido;
+                mecanico.Segundo_Nombre = listaMecanicos[0].Segundo_Nombre;
+                mecanico.Celular = listaMecanicos[0].Celular;
+                mecanico.Direccion = listaMecanicos[0].Direccion;
+                mecanico.Email = listaMecanicos[0].Email;
+
+                return View(mecanico);
+            }
+            //return Json(Respuesta, JsonRequestBehavior.AllowGet);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MecanicosDetail(WebMVC.Models.EditMecanicos mecanico)
+        {
+            string url = ConfigurationManager.AppSettings.Get("WebApiURL")+"mecanicos";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            string json = JsonConvert.SerializeObject(mecanico, Formatting.Indented);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null) return View();
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        string responseBody = objReader.ReadToEnd();
+                        
+                        Console.WriteLine(responseBody);
+                    }
+                }
+            }
+
+
+
+            return RedirectToAction("Index");
+        }
+            //return Json(Respuesta, JsonRequestBehavior.AllowGet);
     }
+
+
 }
---
+++ WebMVC/Models/EditMecanicos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class EditMecanicos
+    {
+        [Required]
+        public string Tipo_Documento { get; set; }
+        [Required]
+        public int Documento { get; set; }
+        [Required]
+        public string Primer_Nombre { get; set; }
+        [Required]
+        public string Segundo_Nombre { get; set; }
+        [Required]
+        public string Primer_Apellido { get; set; }
+        [Required]
+        public string Segundo_Apellido { get; set; }
+        [Required]
+        public string Celular { get; set; }
+        [Required]
+        public string Direccion { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
